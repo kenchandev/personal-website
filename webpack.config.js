1ff1faa0e27d1428303714b98ac1e13f17050593
Ken Chan <thiskenchan@gmail.com>
2020-01-02 01:15:55 -0500

    updated file-loader and mini-css-extract-plugin. resolved incorrect font and image paths in production build.

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,8 @@ const webpackMerge = require("webpack-merge");
 const webpackCommonConfig = require("./webpack.common.config");
 const { NODE_ENV } = process.env;
 const isProd = NODE_ENV === "production";
-const isEnvDefined = !!NODE_ENV;
 const envConfig = isProd
   ? require("./webpack.prod.config")
   : require("./webpack.dev.config");
 
-module.exports = webpackMerge(
-  webpackCommonConfig(isProd, isEnvDefined),
-  envConfig()
-);
+module.exports = webpackMerge(webpackCommonConfig(isProd), envConfig());
