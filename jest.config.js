87cc098c94f65ec40ddffcbd7350437faa8b8b27
Ken Chan <thiskenchan@gmail.com>
2020-01-16 02:02:47 -0500

    added coverage report generation and related configs, added codecov to travis, added lint-staged to perform tests only on recently modified files to be staged, added additional test npm scripts, enhanced jest watch mode with typeahead plugin for pattern matching file and test names

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,19 @@ module.exports = {
   displayName: "client",
   moduleFileExtensions: ["js"],
   testEnvironment: "jest-environment-jsdom",
-  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"]
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  coverageThreshold: {
+    global: {
+      statements: 4,
+      branches: 7,
+      functions: 6,
+      lines: 4
+    }
+  },
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname"
+  ],
+  collectCoverageFrom: ["<rootDir>/assets/scripts/**/*.js"],
+  coveragePathIgnorePatterns: ["__fixtures__"]
 };
