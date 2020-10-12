c5effc9199f708a45cc4efb72d312e74f617b14d
Ken Chan <thiskenchan@gmail.com>
2020-10-11 22:26:32 -0400

    added build job for dev site, changed contact endpoint from default aws domain to custom domain, added robots meta tag to instruct search engines to not show dev page in search results

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,9 @@ const webpackMerge = require("webpack-merge");
 const webpackCommonConfig = require("./webpack.common.config");
 const { NODE_ENV } = process.env;
 const isProd = NODE_ENV === "production";
+const isDev = NODE_ENV === "development";
 const envConfig = isProd
   ? require("./webpack.prod.config")
   : require("./webpack.dev.config");
 
-module.exports = webpackMerge(webpackCommonConfig(isProd), envConfig());
+module.exports = webpackMerge(webpackCommonConfig(isProd, isDev), envConfig());
